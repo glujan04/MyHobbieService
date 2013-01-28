4b85790d14fc3c4663e7bab2f996dfbb03b6035a
Gary Lujan <glujantel@gmail.com>
2013-01-28 04:23:35 -0500

    primer commit

---
+++ MyHobbie.Service/ITocarGuitarraService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyHobbie.Service
+{
+    [ServiceContract]
+    public interface ITocarGuitarraService
+    {
+        [OperationContract]
+        void Registrar(string MarcaGuitarra, string TipoGuitarra);
+
+        [OperationContract]
+        void Listar();
+    }
+}
---
+++ MyHobbie.Service/TocarGuitarraService.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyHobbie.Service
+{
+    public class TocarGuitarraService : ITocarGuitarraService
+    {
+        public void Registrar(string MarcaGuitarra, string TipoGuitarra)
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+                throw new Exception("No se puede registrar nombre igual de la guitarra");
+            }
+        }
+
+        public void Listar()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+                throw new Exception("Ocurrio un error al mostrar el listado.");
+            }
+        }
+    }
+}
